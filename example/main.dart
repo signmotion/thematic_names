9183be1194c30d387d0a81b6d8761df21c48aca2
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-29 12:25:37 +0100

    Updated the example.

--- example/main.dart
+++ example/main.dart
@@ -1,13 +1,16 @@
 // ignore_for_file: avoid_print
 
+import 'dart:io';
+
 import 'package:thematic_names/thematic_names.dart';
 
 void main() {
   // get a set of 1000 unique volcano names
-  final names = Names.volcanos(languageCode: SupportedLanguage.english);
+  final names = Names.stars(languageCode: SupportedLanguage.english);
 
   // all 1000 names
-  print(names);
+  final titles = names.all.map((r) => r.title);
+  File('all_names.txt').writeAsStringSync(titles.join(', '));
 
   // generate 4 names
   for (var i = 0; i < 4; ++i) {
